730453b2d9fd7524cd76442bf788fe49c0d2922e
Andrei <claudiu.andrei.roman@gmail.com>
2019-11-20 15:26:53 +0100

    Provide additional methods for PutsColor module

--- lib/puts_utils/puts_color.rb
+++ lib/puts_utils/puts_color.rb
@@ -12,5 +12,47 @@ module PutsUtils
     def puts_color(string, color = :blue)
       puts "\e[#{COLOR_CODES[color]}m#{string}\e[0m"
     end
+
+    def puts_color_red(*strings)
+      strings.each do |string|
+        puts "\e[0;31m" + string + "\e[0m"
+      end
+    end
+
+    def puts_color_green(*strings)
+      strings.each do |string|
+        puts "\e[0;32m" + string + "\e[0m"
+      end
+    end
+
+    def puts_color_yellow(*strings)
+      strings.each do |string|
+        puts "\e[0;33m" + string + "\e[0m"
+      end
+    end
+
+    def puts_color_blue(*strings)
+      strings.each do |string|
+        puts "\e[0;34m" + string + "\e[0m"
+      end
+    end
+
+    def puts_color_purple(*strings)
+      strings.each do |string|
+        puts "\e[0;35m" + string + "\e[0m"
+      end
+    end
+
+    def puts_color_cyan(*strings)
+      strings.each do |string|
+        puts "\e[0;36m" + string + "\e[0m"
+      end
+    end
+
+    def puts_color_grey(*strings)
+      strings.each do |string|
+        puts "\e[0;37m" + string + "\e[0m"
+      end
+    end
   end
 end
