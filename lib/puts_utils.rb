1f7ed7e7e288224e6df97c8d8aaf48dead475e4b
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2019-11-22 10:51:00 +0100

    Migrates puts_color helper from an internal codebase

--- lib/puts_utils.rb
+++ lib/puts_utils.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 require 'puts_utils/version'
+require 'puts_utils/puts_color'
 
 module PutsUtils
-  class Error < StandardError; end
-  # Your code goes here...
 end
